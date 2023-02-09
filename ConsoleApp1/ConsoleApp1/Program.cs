d35fe6269625af2275c7e109165cca2a04f60373
st0waway <lumixbd@gmail.com>
2023-02-09 22:16:20 +0200

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IBird bird = new Penguin();
+
+			bird.Fly();
+		}
+
+		interface IBird
+		{
+			int LayEggs();
+			void Fly();
+		}
+
+		class Hawk : IBird
+		{
+			public int LayEggs() { return 1; }
+
+			public void Fly()
+			{
+				Console.WriteLine("Hawk flies");
+			}
+		}
+
+		class Penguin : IBird
+		{
+			public int LayEggs() { return 1; }
+			public void Fly() => throw new NotImplementedException();
+		}
+	}
+}
